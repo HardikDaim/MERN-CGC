fce20cd43cbd1ff52ef231b25c0ea0817cc47be3
Hardik Daim <hardikdaim@Hardiks-MacBook-Air.local>
2024-01-20 15:36:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  const data = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Grapes",
+    "Strawberry",
+    "Watermelon",
+    "Pineapple",
+    "Mango",
+    "Cherry",
+    "Blueberry",
+    "Kiwi",
+    "Peach",
+    "Plum",
+    "Raspberry",
+    "Blackberry",
+  ];
 
-function App() {
+  const numbers = [
+    12, 5, 27, 8, 42, 19, 3, 35, 14, 50, 1, 23, 7, 31, 10, 46, 18, 9, 38, 20,
+    49, 15, 4, 26, 33, 11, 29, 6, 45, 17, 36, 22, 2, 30, 48, 16, 40, 13, 25, 37
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Program to Take an array of String and convert to UpperCase */}
+      <h1>Array of Strings</h1>
+      <ol>
+        {data.map((item, index) => {
+          return <li key={index}>{item.toUpperCase()}</li>;
+        })}
+      </ol>
+      {/* Program to print Even numbers from Array */}
+      <h1>Array of Even Numbers</h1>
+      <ol>
+        {numbers.map((item, index) => {
+          return item % 2 === 0 ? <li key={index}>{item}</li> : null;
+        })}
+      </ol>
+    </>
   );
 }
-
-export default App;
